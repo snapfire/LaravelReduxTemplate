fc0375493bfe07f89b9b68d5cd53c5db260d2596
Valentin Mouget <mouget.valentin@gmail.com>
2022-07-31 10:23:38 +0200

    First

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Laravel</title>
+</head>
+<body>
+    <div id="root"></div>
+    @vite('resources/js/index.tsx')
+</body>
+</html>
