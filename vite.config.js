e7d033feda7309e62cc2833c26710ddab90dcd31
Valentin Mouget <mouget.valentin@gmail.com>
2022-07-31 17:59:23 +0200

    Add ESLint

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
     plugins: [
+        eslint(),
         laravel({
             input: ['resources/js/index.tsx'],
             refresh: true,
